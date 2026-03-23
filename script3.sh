46f279c2119b4eba91d4e65045b72411dd958d98
sahil24bcy10092-coder <sahil.24bcy10092@vitbhopal.ac.in>
2026-03-24 01:39:39 +0530

    Update script3.sh

--- script3.sh
+++ script3.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-echo "This is script 3"
+# Script 3: Disk and Permission Auditor
+
+echo "===== Directory Audit ====="
+
+for dir in /etc /home /usr /tmp
+do
+    if [ -d "$dir" ]
+    then
+        echo "Directory: $dir"
+        ls -ld $dir
+        du -sh $dir 2>/dev/null
+        echo "-------------------------"
+    fi
+done
