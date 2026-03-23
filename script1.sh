be26bfd114fc39462ac5a1f74972d34d16a496e8
sahil24bcy10092-coder <sahil.24bcy10092@vitbhopal.ac.in>
2026-03-24 01:38:24 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-echo "Hello from Script 1"
+# Script 1: System Identity Report
+
+echo "===== System Identity Report ====="
+
+echo "User: $(whoami)"
+echo "Kernel Version: $(uname -r)"
+echo "System Uptime: $(uptime -p)"
+echo "Current Date: $(date)"
+echo "License: GNU GPL"
